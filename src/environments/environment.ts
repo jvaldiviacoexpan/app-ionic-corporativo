8f87e0170fdbacd2a4fd3133892124f74ef43d33
jvaldiviacoexpan <jordan.valdivia@coexpan.cl>
2022-02-25 00:03:58 -0300

    FIX | Mejora de variables de entorno

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,17 @@
 
 export const environment = {
   production: false,
-  // urlBase:     'http://localhost:8100',
+  // urlBase:      'https://movil.coexpan.cl/app-corporativo',  // PRODUCCION
+  urlBase: 'https://movil.coexpan.cl/app-corporativo-desa',     // DESARROLLO
+  // urlBase:     'http://localhost:8100',                      // LOCAL
+
+
   // urlcxp:      'https://localhost:44356/api',
   // urlCxpAuth0: 'https://localhost:44360/api',
   // https://localhost:44333/api/v1/logistica/inventario/
   // urlcxpprueba: 'https://localhost:44303',
-  urlBase:      'https://movil.coexpan.cl/app-corporativo-desa',
 
-  // urlBase:      'https://movil.coexpan.cl/app-corporativo',
+
   urlcxp:       'https://api.coexpan.cl/api/v1/bodega/materias-primas',
   urlAuth0:     'https://coexpancl.us.auth0.com',
   urlCxpAuth0:  'https://api.coexpan.cl/api/v1/auth0',
