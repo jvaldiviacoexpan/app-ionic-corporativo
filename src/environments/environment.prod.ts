be7d144a416e00129a5502b440517511d789d52c
jvaldiviacoexpan <jordan.valdivia@coexpan.cl>
2022-10-06 11:10:06 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+
+  // API
+  urlBase:      'https://movil.coexpan.cl/app-corporativo',
+  urlcxp:       'https://api.coexpan.cl/api/v1/bodega/materias-primas',
+  urlAuth0:     'https://coexpancl.us.auth0.com',
+  urlCxpAuth0:  'https://api.coexpan.cl/api/v1/auth0',
+  urlApi:       'https://api.coexpan.cl',
+
+  // DB
+  dbCoexpan: 'SBO_COEMBAL_FUSION',
+  dbCoembal: 'SBO_COEMBAL_FUSION',
 };
